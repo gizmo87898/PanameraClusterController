09931692c0412996749f8022290fa8d154a05b26
gizmo <73662348+gizmo87898@users.noreply.github.com>
2024-07-09 22:43:54 -0400

    Update Panamera.py

--- Panamera.py
+++ Panamera.py
@@ -18,10 +18,8 @@ start_time_100ms = time.time()
 start_time_10ms = time.time()
 start_time_5s = time.time()
 
-id_counter = 0x300
-
+id_counter = 0x640
 counter_4bit = 0
-
 
 ignition = True
 rpm = 780
@@ -57,10 +55,41 @@ trunk = False
 airbag = False
 seatbelt = False
 
+# Global variable for steering wheel control data
+steering_wheel_data = [0, 0, 0]
 
 def gui_thread():
+    def set_steering_wheel_data(data):
+        global steering_wheel_data
+        steering_wheel_data = data
+
+    def reset_steering_wheel_data(event):
+        global steering_wheel_data
+        steering_wheel_data = [0, 0, random.randint(0,255)]
+
     root = tk.Tk()
     root.title("G30")
+
+    button_up = tk.Button(root, text="Up")
+    button_up.bind("<ButtonPress>", lambda event: set_steering_wheel_data([0x12, 0x00, 0x01]))
+    button_up.bind("<ButtonRelease>", reset_steering_wheel_data)
+    button_up.pack()
+
+    button_ok = tk.Button(root, text="OK")
+    button_ok.bind("<ButtonPress>", lambda event: set_steering_wheel_data([0x00, 0x13, 0x10]))
+    button_ok.bind("<ButtonRelease>", reset_steering_wheel_data)
+    button_ok.pack()
+
+    button_down = tk.Button(root, text="Down")
+    button_down.bind("<ButtonPress>", lambda event: set_steering_wheel_data([0x12, 0x00, 0x0F]))
+    button_down.bind("<ButtonRelease>", reset_steering_wheel_data)
+    button_down.pack()
+
+    button_return = tk.Button(root, text="Return")
+    button_return.bind("<ButtonPress>", lambda event: set_steering_wheel_data([0x03, 0x13, 0x01]))
+    button_return.bind("<ButtonRelease>", reset_steering_wheel_data)
+    button_return.pack()
+
     root.mainloop()
 
 # Start the GUI thread
@@ -71,85 +100,45 @@ def receive():
     while True:
         message = bus.recv()
 
-        #print(message)
-
-receive = threading.Thread(target=receive)    
-receive.start()
-
+receive_thread = threading.Thread(target=receive)    
+receive_thread.start()
 
 while True:
     current_time = time.time()
     
-    #read from the socket if there is data to be read
-    #i have no idea how it works, i just know it works
+    # Read from the socket if there is data to be read
     ready_to_read, _, _ = select.select([sock], [], [], 0)
     if sock in ready_to_read:
         data, _ = sock.recvfrom(256)
-        # I4sHc2c7f2I3f16s16si
         packet = struct.unpack('2c7f2I3f', data)
         rpm = int(max(min(packet[3], 8000), 0))
-        speed = packet[2] #convert speed to km/h
+        speed = packet[2] # Convert speed to km/h
         coolant_temp = int(packet[5])
         oil_temp = int(packet[8])
         fuel = int(packet[6]*100)
         gearSelector = packet[0]
         gear = packet[1]
         boost = packet[4]
-        shiftlight = False
-        reverse = False
-        left_directional = False
-        right_directional = False
-        highbeam = False
-        abs_active = False
-        abs_fault = False
-        battery = False
-        tc_active = False
-        tc_off = False
-        cruise_control_active = False
-        handbrake = False
-        shiftlight = False
-        ignition = False
-        lowpressure = False
-        check_engine = False
-        foglight = False
-        lowbeam = False
         
-        if (packet[10]>>0)&1:
-            shiftlight = True
-        if (packet[10]>>1)&1:
-            highbeam = True
-        if (packet[10]>>2)&1:
-            handbrake = True
-        if (packet[10]>>4)&1:
-            tc_active = True
-        if (packet[10]>>5)&1:
-            tc_off = True
-        if (packet[10]>>6)&1:
-            left_directional = True
-        if (packet[10]>>7)&1:
-            right_directional = True
-        if (packet[10]>>8)&1:
-            lowoilpressure = True
-        if (packet[10]>>9)&1:
-            battery = True
-        if (packet[10]>>10)&1:
-            abs_active = True
-        if (packet[10]>>11)&1:
-            abs_fault = True
-        if (packet[10]>>12)&1:
-            ignition = True
-        if (packet[10]>>13)&1:
-            lowpressure = True
-        if (packet[10]>>14)&1:
-            check_engine = True
-        if (packet[10]>>15)&1:
-            foglight = True
-        if (packet[10]>>16)&1:
-            lowbeam = True
-        if (packet[10]>>17)&1:
-            cruise_control_active = True
-
-        #print(bc)
+        # Parse other packet data (bitwise operations)
+        shiftlight = (packet[10]>>0) & 1
+        highbeam = (packet[10]>>1) & 1
+        handbrake = (packet[10]>>2) & 1
+        tc_active = (packet[10]>>4) & 1
+        tc_off = (packet[10]>>5) & 1
+        left_directional = (packet[10]>>6) & 1
+        right_directional = (packet[10]>>7) & 1
+        lowoilpressure = (packet[10]>>8) & 1
+        battery = (packet[10]>>9) & 1
+        abs_active = (packet[10]>>10) & 1
+        abs_fault = (packet[10]>>11) & 1
+        ignition = (packet[10]>>12) & 1
+        lowpressure = (packet[10]>>13) & 1
+        check_engine = (packet[10]>>14) & 1
+        foglight = (packet[10]>>15) & 1
+        lowbeam = (packet[10]>>16) & 1
+        cruise_control_active = (packet[10]>>17) & 1
+    
     # Send each message every 100ms
     elapsed_time_100ms = current_time - start_time_100ms
     if elapsed_time_100ms >= 0.1:
@@ -169,30 +158,29 @@ while True:
                 gearByte,0x50,0,0,0,0,0,0], is_extended_id=False),
             can.Message(arbitration_id=0x662, data=[ # Lighting
                 0x00,0,0b00000011,0,0,0,0,0], is_extended_id=False),
+            can.Message(arbitration_id=0x663, data=[ # Alternator and voltage
+                0,0,0,0,0,0,0,0], is_extended_id=False),
             can.Message(arbitration_id=0x105, data=[ # Speed
                 0x00,0,int(speed*2),0,0,int(speed*2),0,0], is_extended_id=False),
             can.Message(arbitration_id=0x6b5, data=[ # Oil Temp
                 0,0,0,0,0,128 + int((oil_temp*(9/5)+32 - 155) * (80 - 1) / (300 - 155) + 1),0,0], is_extended_id=False),
             can.Message(arbitration_id=0x522, data=[ # CEL, Oil Pressure, Coolant Temp
-                0,0,0,0,(int(boost*1.75)&0b111111)<<2,int(coolant_temp*1.75)&0xf,0x10,0], is_extended_id=False),
+                0,0,0,0,(int(boost*1.75)&0b111111)<<2,min(int(coolant_temp*2),255),0x10,0], is_extended_id=False),
             can.Message(arbitration_id=0x677, data=[ # drivemode, outside temp
                 0,0,0,0,0,0,210,0], is_extended_id=False),
+            can.Message(arbitration_id=0x5bf, data=steering_wheel_data, is_extended_id=False),
             can.Message(arbitration_id=id_counter, data=[
-                random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255),random.randint(0,255)], is_extended_id=False),
+                random.randint(0,255),random.randint(0,255),random.randint(0,255)], is_extended_id=False),
         ]
         
-        #Update checksums and counters here
+        # Update checksums and counters here
         counter_4bit = (counter_4bit + 1) % 16
 
         # Send Messages
         for message in messages_100ms:
             bus.send(message)
-            #print(message)
-            #if message.arbitration_id == 0x1ee:
-            #    print(message)
             wpt.sleep(0.001)
         start_time_100ms = time.time()
-
 
     # Execute code every 10ms
     elapsed_time_10ms = current_time - start_time_10ms
@@ -200,9 +188,8 @@ while True:
         messages_10ms = [
             can.Message(arbitration_id=0x3a3, data=[ # RPM
                 0,0,int(rpm/3)&0xff,int(rpm/3)>>8,0,0,0,0], is_extended_id=False),    
-            
         ]
-        #do checksums here
+        # Do checksums here
 
         for message in messages_10ms:
             bus.send(message)
@@ -211,7 +198,7 @@ while True:
 
     # Execute code every 5s
     elapsed_time_5s = current_time - start_time_5s
-    if elapsed_time_5s >= 3:
+    if elapsed_time_5s >= 6:
         id_counter += 1
         print(hex(id_counter))
         if id_counter == 0x7ff:
@@ -219,7 +206,6 @@ while True:
 
         start_time_5s = time.time()
 
-send_thread_20ms.join()
+receive_thread.join()
 
 sock.close()
-
